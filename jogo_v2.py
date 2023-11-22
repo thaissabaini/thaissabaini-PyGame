79cb0223b5be8a29cd1ce5891e487bc8492bd666
thaissabaini <thaissc3@al.insper.edu.br>
2023-11-22 16:57:47 -0300

    atualizando

--- jogo_v2.py
+++ jogo_v2.py
@@ -5,22 +5,44 @@ from config import altura, largura
 
 pygame.init()
 
-window = pygame.display.set_mode((largura, altura))
+janela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Under the sea')
 
-
 sereia_largura = 100
-sereia_altura = 150
-velocidade_sereia = 10
+sereia_altura = 110
 
+velocidade_sereia = 5
 
 font = pygame.font.SysFont(None, 48)
-background = pygame.image.load('imagens/mar.png').convert()
-background = pygame.transform.scale(background, (largura, altura))
-fundo_rect = background.get_rect()
+fundo = pygame.image.load('imagens/mar.png')
+fundo = pygame.transform.scale(fundo, (largura, altura))
 
 sereia_img = pygame.image.load('imagens/sereia2.png').convert_alpha()
-sereia_img_real = pygame.transform.scale(sereia_img, (sereia_largura, sereia_altura))
+sereia_real = pygame.transform.scale(sereia_img, (sereia_largura, sereia_altura))
+
+tela_inicio = pygame.image.load('imagens/mar.png').convert_alpha()
+tela_inicio = pygame.transform.scale(tela_inicio, (largura, altura))
+
+def tela_inicial():
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_SPACE:
+                    return
+
+        janela.blit(tela_inicio, (0, 0))
+        pygame.display.update()
+
+
+def begin():
+    global jogador, todas_sprites
+
+    jogador = Sereia(sereia_real)
+    todas_sprites = pygame.sprite.Group()
+    todas_sprites.add(jogador)
 
 
 class Sereia(pygame.sprite.Sprite):
@@ -33,26 +55,33 @@ class Sereia(pygame.sprite.Sprite):
         self.speedx = 0
         self.speedy = 0
 
+        self.velocidade_y = 0
+
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top < 0:
-            self.rect.top = 0
+
         if self.rect.bottom > altura:
             self.rect.bottom = altura
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+
         if self.rect.right > largura:
             self.rect.right = largura
         if self.rect.left < 0:
             self.rect.left = 0
 
+
 game = True
 clock = pygame.time.Clock()
-FPS = 30
+FPS = 60
 
 todas_sprites = pygame.sprite.Group()
-jogador = Sereia(sereia_img_real)
+jogador = Sereia(sereia_real)
 todas_sprites.add(jogador)
 
+tela_inicial()
 
 while game:
     clock.tick(FPS)
@@ -82,9 +111,11 @@ while game:
                 jogador.speedy = 0
 
     todas_sprites.update()
-    window.blit(background, fundo_rect)
 
-    todas_sprites.draw(window)
+    janela.fill((255, 255, 255))
+    janela.blit(fundo, (0, 0))
+
+    todas_sprites.draw(janela)
     pygame.display.update()
 
 pygame.quit()
